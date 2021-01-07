148b05d15aa27e2d4be25d35267839bc75a07072
Denis Pereira Raymundo <denisuba@gmail.com>
2021-01-07 16:20:30 -0300

    Teste Microsserviço

---
+++ Pascal/Joao14GitHub.pas
@@ -0,0 +1,40 @@
+unit Joao14GitHub;
+
+function Main: String;
+begin
+  Result :=   
+    '<h1>João 14</h1>' + #13 + 
+    '<p>1 Não se turbe o vosso coração; credes em Deus, crede também em mim.' + #13 + 
+    '<p>2 Na casa de meu Pai há muitas moradas; se não fosse assim, eu vo-lo teria dito. Vou preparar-vos lugar.' + #13 + 
+    '<p>3 E quando eu for, e vos preparar lugar, virei outra vez, e vos levarei para mim mesmo, para que onde eu estiver estejais vós também.' + #13 + 
+    '<p>4 Mesmo vós sabeis para onde vou, e conheceis o caminho.' + #13 +
+    '<p>5 Disse-lhe Tomé: Senhor, nós não sabemos para onde vais; e como podemos saber o caminho?' + #13 + 
+    '<p>6 Disse-lhe Jesus: Eu sou o caminho, e a verdade e a vida; ninguém vem ao Pai, senão por mim. ' + #13 + 
+    '<p>7 Se vós me conhecêsseis a mim, também conheceríeis a meu Pai; e já desde agora o conheceis, e o tendes visto.' + #13 + 
+    '<p>8 Disse-lhe Filipe: Senhor, mostra-nos o Pai, o que nos basta.' + #13 +
+    '<p>9 Disse-lhe Jesus: Estou há tanto tempo convosco, e não me tendes conhecido, Filipe? Quem me vê a mim vê o Pai; e como dizes tu: Mostra-nos o Pai?' + #13 +
+    '<p>10 Não crês tu que eu estou no Pai, e que o Pai está em mim? As palavras que eu vos digo não as digo de mim mesmo, mas o Pai, que está em mim, é quem faz as obras.' + #13 + 
+    '<p>11 Crede-me que estou no Pai, e o Pai em mim; crede-me, ao menos, por causa das mesmas obras.' + #13 + 
+    '<p>12 Na verdade, na verdade vos digo que aquele que crê em mim também fará as obras que eu faço, e as fará maiores do que estas, porque eu vou para meu Pai.' + #13 + 
+    '<p>13 E tudo quanto pedirdes em meu nome eu o farei, para que o Pai seja glorificado no Filho.' + #13 +
+    '<p>14 Se pedirdes alguma coisa em meu nome, eu o farei.' + #13 +
+    '<p>15 Se me amais, guardai os meus mandamentos.' + #13 +
+    '<p>16 E eu rogarei ao Pai, e ele vos dará outro Consolador, para que fique convosco para sempre;' + #13 +
+    '<p>17 O Espírito de verdade, que o mundo não pode receber, porque não o vê nem o conhece; mas vós o conheceis, porque habita convosco, e estará em vós.' + #13 +
+    '<p>18 Não vos deixarei órfãos; voltarei para vós.' + #13 +
+    '<p>19 Ainda um pouco, e o mundo não me verá mais, mas vós me vereis; porque eu vivo, e vós vivereis.' + #13 +
+    '<p>20 Naquele dia conhecereis que estou em meu Pai, e vós em mim, e eu em vós.' + #13 +
+    '<p>21 Aquele que tem os meus mandamentos e os guarda esse é o que me ama; e aquele que me ama será amado de meu Pai, e eu o amarei, e me manifestarei a ele.' + #13 +
+    '<p>22 Disse-lhe Judas (não o Iscariotes): Senhor, de onde vem que te hás de manifestar a nós, e não ao mundo?' + #13 +
+    '<p>23 Jesus respondeu, e disse-lhe: Se alguém me ama, guardará a minha palavra, e meu Pai o amará, e viremos para ele, e faremos nele morada.' + #13 +
+    '<p>24 Quem não me ama não guarda as minhas palavras; ora, a palavra que ouvistes não é minha, mas do Pai que me enviou.' + #13 +
+    '<p>25 Tenho-vos dito isto, estando convosco.' + #13 +
+    '<p>26 Mas aquele Consolador, o Espírito Santo, que o Pai enviará em meu nome, esse vos ensinará todas as coisas, e vos fará lembrar de tudo quanto vos tenho dito.' + #13 +
+    '<p>27 Deixo-vos a paz, a minha paz vos dou; não vo-la dou como o mundo a dá. Não se turbe o vosso coração, nem se atemorize.' + #13 +
+    '<p>28 Ouvistes que eu vos disse: Vou, e venho para vós. Se me amásseis, certamente exultaríeis porque eu disse: Vou para o Pai; porque meu Pai é maior do que eu.' + #13 +
+    '<p>29 Eu vo-lo disse agora antes que aconteça, para que, quando acontecer, vós acrediteis.' + #13 +
+    '<p>30 Já não falarei muito convosco, porque se aproxima o príncipe deste mundo, e nada tem em mim;' + #13 +
+    '<p>31 Mas é para que o mundo saiba que eu amo o Pai, e que faço como o Pai me mandou. Levantai-vos, vamo-nos daqui.';  
+end;
+
+end.
